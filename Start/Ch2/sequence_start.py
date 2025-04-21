05bc8494a5fa19c040659692e967b081a7409646
locococobanana <142081518+locococobanana@users.noreply.github.com>
2025-04-21 13:52:57 -0600

    Update sequence_start.py

--- Start/Ch2/sequence_start.py
+++ Start/Ch2/sequence_start.py
@@ -3,13 +3,19 @@
 
 # Sequences: Lists and Tuples
 # These are -- surprise -- sequences of values
-
+mylist = [0, 1, "two", 3.2, False]
+# print(len(mylist))
 
 # to access a member of a sequence type, use []
-
-
+# print(mylist[0])
+# print(mylist[-3])
+# mylist[0] = 10
+# print(mylist)
+# print(mylist[0])
 # add a list to another list
-
+another_list = [6,7,8]
+mylist = another_list + mylist
+print(mylist)
 
 # use slices to get parts of a sequence
 
