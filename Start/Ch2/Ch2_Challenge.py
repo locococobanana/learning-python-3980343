533835e07e6d8783017685573a483e4abf1804a6
locococobanana <142081518+locococobanana@users.noreply.github.com>
2025-09-24 16:35:12 -0600

    Ch2

---
+++ Start/Ch2/Ch2_Challenge.py
@@ -0,0 +1,20 @@
+numbers = [7, 17, 2, 13, 19, 20, 0, 5, 11, 1280, 105]
+
+result1 = Answer.count_numbers("even", numbers)
+result2 = Answer.count_numbers("odd", numbers)
+result3 = Answer.count_numbers("Blarg", numbers)
+
+def count_numbers(which, numbers):
+    # Your code goes here
+
+    return -1
+
+# count the even numbers in the list
+
+
+
+# count the odd numbers in the list
+
+
+
+# count the numbers in the list
--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -3,20 +3,45 @@
 
 
 # define a basic function
-def hello_func():
-  print("Hello World!")
-  name = input("What is your name? ")
-  print("Nice to meet you,",name+"!")
+# def hello_func():
+#   print("Hello World!")
+#   name = input("What is your name? ")
+#   print("Nice to meet you,",name+"!")
 
-hello_func()
+# hello_func()
 
-# function that takes parameters
+# function that takes parameters (allows you to customize the action)
+# def hello_func(greeting):
+#   print("Hello World!")
+#   name = input("What is your name? ")
+#   print(greeting,name)
 
+# hello_func("How are you doing")
+# hello_func("Hey, what's up")
 
 # function that returns a value
+# def cube(x):
+#   return x * x * x
 
+# result = cube(3)
+# print(result)
 
 # function with default value for an parameter
+# def hello_func(greeting, name=None):
+#   print("Hello, World!")
+#   if name == None:
+#     name = input("What is your name? ")
+#   print(greeting,name+"!")
 
+# hello_func("Nice to meet you,")
+
+# can change the order by explicitly assigning the variable
+# hello_func(name="Joe", greeting="nice to meet you")
 
 # function with variable number of parameters
+def multi_add(start, *args):
+    result = start
+    for x in args:
+        result = result + x
+    return result
+print(multi_add(0,4,5,10,4,10))
